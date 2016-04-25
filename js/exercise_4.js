1e06975d32767e84516107d08ae799da4635db40
rshouser16 <rhouser@ku.edu>
2016-04-25 10:05:41 -0500

    Updated exercise_4.js

--- js/exercise_4.js
+++ js/exercise_4.js
@@ -33,6 +33,6 @@ featureLayer.on('ready', function() {
 
 featureLayer.on('ready', function(){
   this.eachLayer(function(layer){
-    layer.bindPopup('Welcome to ' + Layer.feature.properties.LABEL);
+    layer.bindPopup('Welcome to ' + layer.feature.properties.LABEL);
 })
   })
