01bf45a7585ee83b0498dbc7c6f517b91dc7ca77
rshouser16 <rhouser@ku.edu>
2016-04-25 10:04:40 -0500

    Updated exercise_4.js

--- js/exercise_4.js
+++ js/exercise_4.js
@@ -31,7 +31,7 @@ featureLayer.on('ready', function() {
     map.fitBounds(featureLayer.getBounds());
 })
 
-feature.Layer.on('ready', function(){
+featureLayer.on('ready', function(){
   this.eachLayer(function(layer){
     layer.bindPopup('Welcome to ' + Layer.feature.properties.LABEL);
 })
